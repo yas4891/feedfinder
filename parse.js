a65566dfb242a59157c808b265c519eb18729d7f
vagrant <engelhardt.christoph@gmx.net>
2013-04-22 11:44:55 -0700

    -add: memwatch -change: extracted most part out into a function

--- parse.js
+++ parse.js
@@ -2,6 +2,31 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 var async = require('async');
 var modurl = require('url');
+var memwatch = require("memwatch");
+
+function parseData(data, screenname) {
+    var line = data.split(/\r?\n/)[0];
+    var url = line.split(" ")[1];
+	    
+    try {
+	$ = cheerio.load(data);
+	    
+	var links = $('link[type*="application/rss"][rel*="alternate"]');
+		
+	links.each(function(index, elt) {
+	    var relurl = $(this).attr('href');
+	    var absurl = modurl.resolve(url, relurl);
+	    fs.appendFile("feeds.txt", url + " >> " + absurl + "\n");
+	});
+    }
+    catch(exception) {
+	console.log("error with screenname:" + screenname);
+	console.log("exception:" + exception);
+    }
+    data = null; 
+    line = null;
+    url	 = null;
+}
 
 var qRdFile = async.queue(function(task, callback) {
     var called = false;
@@ -11,30 +36,7 @@ var qRdFile = async.queue(function(task, callback) {
 		console.log(error);
 		return;
 	    }
-	    
-	    var line = data.split(/\r?\n/)[0];
-	    var url = line.split(" ")[1];
-	    
-		try {
-	    //setImmediate(function() { 
-		$ = cheerio.load(data);
-	    
-		var links = $('link[type*="application/rss"][rel*="alternate"]');
-		
-		links.each(function(index, elt) {
-		    var relurl = $(this).attr('href');
-		    var absurl = modurl.resolve(url, relurl);
-		    fs.appendFile("feeds.txt", url + " >> " + absurl + "\n");
-		});
-
-		}
-		catch(exception) {
-		     
-		    console.log("error with screenname:" + task.filename);
-		    console.log("exception:" + exception);
-		}
-	    //});
-	    /* */
+	    parseData(data, task.filename); 
 	});
 	setImmediate(function() {callback(null);});
 }, 50);
@@ -51,6 +53,12 @@ fs.readdir(dir, function(errRdDir, files) {
 	}); // push queue
     }); // files.forEach
 }); // read directory
+
+memwatch.on('stats', function(info) {
+    console.log(info);
+
+});
+
 
 function fileCallback(errFile) 
 {
